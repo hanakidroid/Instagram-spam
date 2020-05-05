d3a555fc34cc6594e22dff4360faa7f7024fe473
hanakidroid <62693543+hanakidroid@users.noreply.github.com>
2020-05-05 12:58:56 +0700

    Update instaspam.sh

--- instaspam.sh
+++ instaspam.sh
@@ -3,28 +3,26 @@
 # github.com/hanakidroid/instaspam
 # Author: @chrltomoya15_ S0l0 Coder(You don't become a coder by just changing the credits)
 
-csrftoken=$(curl https://www.instagram.com/accounts/login/ajax -L -i -s | grep "csrftoken" | cut -d "=" -f2 | cut -d ";" -f1)
+$curl Hey,There!!
 
 
 login_user() {
 
-if [[ "$default_username" == "" ]]; then
-read -p $'\e[1;77m[\e[0m\e[1;92m+\e[0m\e[1;77m] Login: \e[0m' username
+if  "$default_Ur Name" == "" ; then
+read -p $'\e[1;77m[\e[0m\e[1;92m+\e[0m\e[1;77m] Login: \e[0m' Ur name
 else
-username="${username:-${default_username}}"
+ur name="${ur name:-${default_ur name}}"
 fi
 
-if [[ "$default_password" == "" ]]; then
-read -s -p $'\e[1;77m[\e[0m\e[1;92m+\e[0m\e[1;77m] Password: \e[0m' password
+if  "$default_now,i already found you" == "" ; then
+read -s -p $'\e[1;77m[\e[0m\e[1;92m+\e[0m\e[1;77m] good girl: \e[0m' good girl
 else
-password="${password:-${default_password}}"
+password="${password:-${default_good girl}}"
 fi
 
-printf "\e[\n1;77m[*] Trying to login as\e[0m\e[1;77m %s\e[0m\n" $username
+printf "\e[\n1;77m[*] Trying to open as\e[0m\e[1;77m %s\e[0m\n" $ur name
 IFS=$'\n'
-check_login=$(curl -c cookies.txt 'https://www.instagram.com/accounts/login/ajax/' -H 'Cookie: csrftoken='$csrftoken'' -H 'X-Instagram-AJAX: 1' -H 'Referer: https://www.instagram.com/' -H 'X-CSRFToken:'$csrftoken'' -H 'X-Requested-With: XMLHttpRequest' --data 'username='$username'&password='$password'&intent' -L --compressed -s | grep -o '"authenticated": true')
 
-if [[ "$check_login" == *'"authenticated": true'* ]]; then
 printf "\e[1;92m[*] Login Successful!\e[0m\n"
 else
 printf "\e[1;93m[!] Check your login data or IP! Dont use Tor, VPN, Proxy. It requires your usual IP.\n\e[0m"
@@ -35,29 +33,14 @@ fi
 
 config() {
 
-IFS=$'\n'
-default_amount="100"
-default_message="InstaSpam"
+IFS=$'\n'I Love U="click number 1-100"
+Are u love me too?="Love U"
 read -p $'\e[1;77m[\e[0m\e[1;92m+\e[0m\e[1;77m] Message: ' message
 message="${message:-${default_message}}"
-read -p $'\e[1;77m[\e[0m\e[1;92m+\e[0m\e[1;77m] Amount message (Default: 100): ' amount
+read -p $'\e[1;77m[\e[0m\e[1;92m+\e[0m\e[1;77m] Amount U love me(%) (Default: 100): ' amount
 amount="${amount:-${default_amount}}"
 
-}
-
-
-account() {
-
-read -p $'\e[1;77m[\e[0m\e[1;92m+\e[0m\e[1;77m] Spammed Account: ' account_spam
-
-checkaccount=$(curl -L -s https://www.instagram.com/$account_spam/ | grep -c "the page may have been removed")
-if [[ "$checkaccount" == 1 ]]; then
-printf "\e[1;91mInvalid Username! Try again\e[0m\n"
-sleep 1
-account
-fi
-
-curl -s -L https://www.instagram.com/$account_spam | grep  -o '"id":"..................[0-9]' | cut -d ":" -f2 | tr -d '"' > media_id
+0-9]' | cut -d ":" -f2 | tr -d '"' > media_id
 counter="1"
 for id in $(cat media_id); do
 printf "\e[1;92m%s:\e[0m\e[1;77m %s\e[0m\n" $counter $id
@@ -76,12 +59,10 @@ flood() {
 count=1
 for i in $(seq 1 $amount); do
 
-printf "\e[1;77m[\e[0m\e[1;92m+\e[0m\e[1;77m] Sending message:\e[0m\e[1;93m %s\e[0m\e[1;77m/\e[0m\e[1;93m%s ... \e[0m" $i $amount
+printf "\e[1;77m[\e[0m\e[1;92m+\e[0m\e[1;77m] Makasih udh mau jawab:):\e[0m\e[1;93m %s\e[0m\e[1;77m/\e[0m\e[1;93m%s ... \e[0m" $i $thx u:)
 IFS=$'\n'
 
-comment=$(curl  -i -s -k  -X $'POST'     -H $'Host: www.instagram.com' -H $'User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:52.0) Gecko/20100101 Firefox/52.0' -H $'Accept: */*' -H $'Accept-Language: en-US,en;q=0.5' -H $'Accept-Encoding: gzip, deflate'  -H $'X-CSRFToken:'$csrftoken'' -H $'X-Instagram-AJAX: 9de6d949df8f' -H $'Content-Type: application/x-www-form-urlencoded' -H $'X-Requested-With: XMLHttpRequest' -H $'Cookie: csrftoken='$csrftoken'; ' -H $'Connection: close'     -b cookies.txt     --data-binary $'comment_text='$message' '$count'&replied_to_comment_id='     $'https://www.instagram.com/web/comments/'$media_id'/add/' -w "\n%{http_code}\n" | grep -a "HTTP/2 200"); if [[ "$comment" == *'HTTP/2 200'* ]]; then printf "\e[1;92mOK!\e[0m\n";  printf "%s\n" $media_id >> commented.txt ; else printf "\e[1;93mFAIL!\e[0m \e[1;77mSleeping 120 secs...\e[0m\n"; sleep 120;  fi; 
-sleep 1
-let count++
+
 done
 }
 
@@ -93,15 +74,6 @@ command -v curl > /dev/null 2>&1 || { echo >&2 "I require curl but it's not inst
 
 banner() {
 
-printf "\e[1;77m  _____           _         _____                        \n"
-printf " |_   _|         | |       / ____|                       \n"
-printf "   | |  _ __  ___| |_ __ _| (___  _ __   __ _ _ __ ___   \n"
-printf "   | | | '_ \/ __| __/ _\` |\___ \| '_ \ / _\` | '_ \` _ \  \n"
-printf "  _| |_| | | \__ \ || (_| |____) | |_) | (_| | | | | | | \n"
-printf " |_____|_| |_|___/\__\__,_|_____/| .__/ \__,_|_| |_| |_| \n"
-printf "                                 | |                     \n"
-printf "                                 |_|\e[0m\e[1;92mv1.0 by @chrltomoya15_ S0L0 Coder\e[0m                     \n"
-printf "\n"
 
 }
 
